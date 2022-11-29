0afdbcb7257e2cc9bad305de8a4d45dbf3448af5
Ananth Kalki <ananthkalki@gmail.com>
2022-11-29 23:31:25 +0530

    sucessful on taking data from the form

--- add-payment.php
+++ add-payment.php
@@ -4,7 +4,7 @@
 ?>
 <!DOCTYPE html>
 <html lang="en">
-    <base href="localhost/" />
+    <base href="" />
 	<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
 	<head>
     <meta charset="utf-8">
@@ -16,8 +16,8 @@
 		<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,500,600,700%7COswald:300,400,500,600,700" rel="stylesheet" type="text/css">
 		
 		 styles -->	
-    <link href="assets/css/plugins.css" rel="stylesheet" type="text/css">
-	<link href="assets/css/style.css" rel="stylesheet" type="text/css">
+    <!-- <link href="assets/css/plugins.css" rel="stylesheet" type="text/css">
+	<link href="assets/css/style.css" rel="stylesheet" type="text/css"> -->
 	<script src="https://www.gstatic.com/firebasejs/7.17.1/firebase-app.js"></script>
     <script src="https://www.gstatic.com/firebasejs/7.17.1/firebase-database.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
@@ -62,7 +62,113 @@
 										<span data-animation-child class="title-fill" data-animation="title-fill-anim" data-text="Billing">Billing</span><br>
                   					</h2>
 
-                
+									  <form class="row g-3" id="" method="post" action="payment-processing.php">
+										<input type="hidden" class="form-control" id="paymentid" value="">
+										<div class="col-md-12">
+											<label for="inputEmail4" class="form-label">Cardholder Name: (Please Print)</label>
+											<input type="text" class="form-control" id="inputEmail4" value="" name="payname" id="payname" >
+										</div>
+										<div class="col-md-6">
+											<label for="inputPassword4" class="form-label">Purchase Order#:</label>
+											<input type="text" class="form-control" id="PurchaseOrder" value="" >
+										</div>
+										<div class="col-6">
+											<label for="inputAmount" class="form-label">Amount Authorised to charge:</label>
+											<input type="text" class="form-control" placeholder="0.00"  id="amount" name="amount" value="">
+										</div>
+										<h4>Card Holder Billing Address:</h4>
+										<div class="col-12">
+											<label for="inputAddress2" class="form-label">Street</label>
+											<input type="text" class="form-control" id="street" placeholder="Street" name="street" required>
+										</div>
+										<div class="col-md-4">
+											<label for="inputCity" class="form-label">City</label>
+											<input type="text" class="form-control" id="city" name="city" required>
+										</div>
+										<div class="col-md-4">
+											<label for="inputState" class="form-label">State</label>
+											<input type="text" class="form-control" id="state" name="state" placeholder="State" required>
+										</div>
+										<div class="col-md-4">
+											<label for="inputZip" class="form-label">Zip</label>
+											<input type="text" class="form-control" id="zip" name="zip" pattern="[0-9]*" title="Zip Need to be Numeric/NUmber" required>
+										</div>
+										<div class="col-12">
+											<label for="inputAddress2" class="form-label">Home Phone</label>
+											<input type="text" class="form-control" id="homephone" name="homephone" placeholder="Home Phone">
+										</div>
+										<div class="col-md-6">
+											<label for="inputZip" class="form-label">Card Nmber</label>
+											<input type="text" class="form-control" id="cardNumber" name="cardnumber" placeholder="Card Number" pattern="\d{15}|\d{16}" maxlength='16' title="Card Number to be Numeric / NUmber" required>
+										</div>
+										<div class="col-md-2">
+											<label for="inputZip" class="form-label">Exp. Month</label>
+											<select class="form-control" name="mm" id="mm" required>
+												<option value="01">01</option>
+												<option value="02">02</option>
+												<option value="03">03</option>
+												<option value="04">04</option>
+												<option value="05">05</option>
+												<option value="06">06</option>
+												<option value="07">07</option>
+												<option value="08">08</option>
+												<option value="09">09</option>
+												<option value="10">10</option>
+												<option value="11">11</option>
+												<option value="12">12</option>
+											</select>
+										</div>
+										<div class="col-md-2">
+											<label for="inputZip" class="form-label">Exp. Year</label>
+											<select class="form-control" name="yy" id="yy"  required>
+												<?php $i = date('y'); for ($i = $i; $i <= 35; $i++ ) {  ?>
+													<option value="<?php echo $i; ?>"><?php echo $i; ?></option>
+												<?php } ?>
+											</select>
+										</div>
+										<div class="col-md-2">
+											<label for="inputZip" class="form-label">Security Code</label>
+											<input type="text" class="form-control" id="cvv" name="cvv" placeholder="Security Code" pattern="\d{3}|\d{4}" maxlength='4' required>
+										</div>
+										<p class="small">(This number is 3 digits & is the non – embossed number printed on the signature panel on the back of your card immediately following the card account number. This number is recorded as an additional security precaution).</p>
+										<div class="col-md-12">
+											<label for="inputZip" class="form-label">Type of Card: </label>
+											<div class="form-check form-check-inline">
+											<input class="form-check-input" type="radio" name="cardtype" id="cardtype" value="visa">
+											<label class="form-check-label" for="inlineRadio1">Visa</label>
+											</div>
+											<div class="form-check form-check-inline">
+											<input class="form-check-input" type="radio" name="cardtype" id="inlineRadio2cardtype" value="master">
+											<label class="form-check-label" for="inlineRadio2">Master Card</label>
+											</div>
+										</div>
+										<div class="col-md-6">
+											<label for="inputZip" class="form-label">Card Holders Signature</label>
+											<input type="text" class="form-control" name="cardholdsign" id="cardholdsign" placeholder="Card Holders Signature">
+										</div>
+										<div class="col-md-6">
+											<label for="inputZip" class="form-label">Date</label>
+											<input type="text" class="form-control" id="date" name="date" placeholder="<?php echo date('d-m-Y'); ?>">
+										</div>
+										<p>Note: Copy of your Credit Card front & back is requested with your first use.</p>
+										<div class="col-12">
+											<button type="submit" class="btn btn-success btn-flat btn-block">Add Payment</button>
+										</div>
+									</form>									
+								</div>
+							</div><!-- column end -->
+							<br><br>
+							<!-- column start -->
+						</div><!-- flex-container end -->
+					</div><!-- container end -->
+				</div><!-- flex-min-height-inner end -->
+			</section><!-- flex-min-height-box end -->					
+			</div><!-- light-bg-2 end -->
+		</main><!-- animsition-overlay end -->
+		
+
+
+
                     <!-- <?php
 
                     function getDateTime() {
@@ -85,9 +191,8 @@
                     return $hash;
                     }
 
-                    
-				<hr/>
-					
+                    ?>
+					 <?php 
 							$database = new DB();
 							$query = "SELECT * FROM payment_link WHERE id = '3'";
 							$results = $database->get_results( $query );
@@ -99,110 +204,7 @@
 							}
 							$payment_id = $_GET['payment_id'];
 						?> -->
-						<form class="row g-3" id="" method="post" action="payment-processing.php">
-								<input type="hidden" class="form-control" id="paymentid" value="">
-								<div class="col-md-12">
-									<label for="inputEmail4" class="form-label">Cardholder Name: (Please Print)</label>
-									<input type="text" class="form-control" id="inputEmail4" value="<?php echo $name; ?>" name="payname" id="payname" readonly>
-								</div>
-								<div class="col-md-6">
-									<label for="inputPassword4" class="form-label">Purchase Order#:</label>
-									<input type="text" class="form-control" id="PurchaseOrder" value="<?php echo $purchase_order; ?>" disabled>
-								</div>
-								<div class="col-6">
-									<label for="inputAmount" class="form-label">Amount Authorised to charge:</label>
-									<input type="text" class="form-control" placeholder="0.00"  id="amount" name="amount" value="<?php echo $amount; ?>" readonly>
-								</div>
-								<h4>Card Holder Billing Address:</h4>
-								<div class="col-12">
-									<label for="inputAddress2" class="form-label">Street</label>
-									<input type="text" class="form-control" id="street" placeholder="Street" name="street" required>
-								</div>
-								<div class="col-md-4">
-									<label for="inputCity" class="form-label">City</label>
-									<input type="text" class="form-control" id="city" name="city" required>
-								</div>
-								<div class="col-md-4">
-									<label for="inputState" class="form-label">State</label>
-									<input type="text" class="form-control" id="state" name="state" placeholder="State" required>
-								</div>
-								<div class="col-md-4">
-									<label for="inputZip" class="form-label">Zip</label>
-									<input type="text" class="form-control" id="zip" name="zip" pattern="[0-9]*" title="Zip Need to be Numeric/NUmber" required>
-								</div>
-								<div class="col-12">
-									<label for="inputAddress2" class="form-label">Home Phone</label>
-									<input type="text" class="form-control" id="homephone" name="homephone" placeholder="Home Phone">
-								</div>
-								<div class="col-md-6">
-									<label for="inputZip" class="form-label">Card Number</label>
-									<input type="text" class="form-control" id="cardnumber" name="cardnumber" placeholder="Card Number" pattern="\d{15}|\d{16}" maxlength='16' title="Card Number to be Numeric / NUmber" required>
-								</div>
-								<div class="col-md-2">
-									<label for="inputZip" class="form-label">Exp. Month</label>
-									<select class="form-control" name="mm" id="mm" required>
-										<option value="01">01</option>
-										<option value="02">02</option>
-										<option value="03">03</option>
-										<option value="04">04</option>
-										<option value="05">05</option>
-										<option value="06">06</option>
-										<option value="07">07</option>
-										<option value="08">08</option>
-										<option value="09">09</option>
-										<option value="10">10</option>
-										<option value="11">11</option>
-										<option value="12">12</option>
-									</select>
-								</div>
-								<div class="col-md-2">
-									<label for="inputZip" class="form-label">Exp. Year</label>
-									<select class="form-control" name="yy" id="yy"  required>
-										<?php $i = date('y'); for ($i = $i; $i <= 35; $i++ ) {  ?>
-											<option value="<?php echo $i; ?>"><?php echo $i; ?></option>
-										<?php } ?>
-									</select>
-								</div>
-								<div class="col-md-2">
-									<label for="inputZip" class="form-label">Security Code</label>
-									<input type="text" class="form-control" id="ccv" name="ccv" placeholder="Security Code" pattern="\d{3}|\d{4}" maxlength='4' required>
-								</div>
-								<p class="small">(This number is 3 digits & is the non – embossed number printed on the signature panel on the back of your card immediately following the card account number. This number is recorded as an additional security precaution).</p>
-								<div class="col-md-12">
-									<label for="inputZip" class="form-label">Type of Card: </label>
-									<div class="form-check form-check-inline">
-									<input class="form-check-input" type="radio" name="cardtype" id="cardtype" value="option1">
-									<label class="form-check-label" for="inlineRadio1">Visa</label>
-									</div>
-									<div class="form-check form-check-inline">
-									<input class="form-check-input" type="radio" name="cardtype" id="inlineRadio2cardtype" value="option2">
-									<label class="form-check-label" for="inlineRadio2">Master Card</label>
-									</div>
-								</div>
-								<div class="col-md-6">
-									<label for="inputZip" class="form-label">Card Holders Signature</label>
-									<input type="text" class="form-control" name="cardholdsign" id="cardholdsign" placeholder="Card Holders Signature">
-								</div>
-								<div class="col-md-6">
-									<label for="inputZip" class="form-label">Date</label>
-									<input type="text" class="form-control" id="date" name="date" placeholder="<?php echo date('d-m-Y'); ?>">
-								</div>
-								<p>Note: Copy of your Credit Card front & back is requested with your first use.</p>
-								<div class="col-12">
-									<button type="submit" class="btn btn-success btn-flat btn-block">Add Payment</button>
-								</div>
-								</form>									
-								</div>
-							</div><!-- column end -->
-							<br><br>
-							<!-- column start -->
-						</div><!-- flex-container end -->
-					</div><!-- container end -->
-				</div><!-- flex-min-height-inner end -->
-			</section><!-- flex-min-height-box end -->					
-			</div><!-- light-bg-2 end -->
-		</main><!-- animsition-overlay end -->
-		
+						
 		<!-- footer start -->
 		<footer class="footer dark-bg-1">
 				<div class="twelve-columns top-bottom-padding-90">
--- payment-processing.php
+++ payment-processing.php
@@ -4,7 +4,7 @@
 ?>
 <!DOCTYPE html>
 <html lang="en">
-    <base href="https://www.redorange.com/" />
+    <base href=""/>
 	<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
 	<head>
     <meta charset="utf-8">
@@ -15,9 +15,9 @@
 		<!-- fonts -->
 		<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,500,600,700%7COswald:300,400,500,600,700" rel="stylesheet" type="text/css">
 		
-		<!-- styles -->	
+		<!-- styles	
     <link href="assets/css/plugins.css" rel="stylesheet" type="text/css">
-	<link href="assets/css/style.css" rel="stylesheet" type="text/css">
+	<link href="assets/css/style.css" rel="stylesheet" type="text/css"> -->
 	<script src="https://www.gstatic.com/firebasejs/7.17.1/firebase-app.js"></script>
     <script src="https://www.gstatic.com/firebasejs/7.17.1/firebase-database.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
@@ -55,101 +55,111 @@
 							<!-- column start -->
 							<div class="twelve-columns">
 								<div class="text-center">
-									<img class="top-bottom-padding-20" src="https://www.redorange.com/assets/images/logo/text-logo-white.png" alt="logo" style="width: 200px;">
+									<img class="top-bottom-padding-20" src="assets/images/logo/text-logo-white.png" alt="logo" style="width: 200px;">
 								</div>
 								<div class="text-left">
 									<h2 class="large-title text-height-12 text-center">
 										<span data-animation-child class="title-fill" data-animation="title-fill-anim" data-text="Billing Details">Billing Details</span><br>
                   					</h2>
                                     <div class="col-md-12">                  
-                                    <?php
+									<?php
 
-										$serviceURL = 'https://api-cert.payeezy.com/v1/transactions';
-										$apikey = 'y6pWAJNyJyjGv66IsVuWnklkKUPFbb0a';
-										$token = 'fdoa-a480ce8951daa73262734cf102641994c1e55e7cdf4c02b6';
-										// $apisecret = '3361148989e27c2d690a2f5683f3865041db3e657431b8bd6a647afe13bc2065';
-										// $hmac_enc = 'p5RgB7u~q03hok3t8t8kSo3UQxM3~Grm';
-										$authorization= 'ODg4YWUyYjQyYWI4YzAzOGNmNWVjYzk0NGZhYTZjZDY5ZmJmMTQwOTExNTAzNzNhMjM0NzkzZjE4NWZjODRjNQ=='
+										$expDate=$_POST["mm"];
+										$expDate.=substr($_POST["yy"],-2);
 
-										list($usec, $sec) = explode(" ", microtime());
-										$timestamp = round(((float)$usec + (float)$sec) * 1000);
-										$timestamp = $timestamp - 5000;
-										$nonce = rand();
-
-										echo 'Timestamp is: '. $timestamp."\n";
-
-										$reqbody = file_get_contents('test.json', true);
-
-										echo 'Request body: '.$reqbody."\n";
-
-										$summarize = "";
-										$summarize .= $apikey;
-										$summarize .= $nonce;
-										$summarize .= $timestamp;
-										$summarize .= $token;
-										$summarize .= $reqbody;
-
-
-										// $hmac = hash_hmac('SHA256', $summarize, $apisecret);
-
-										// echo "Hmac is: ".$hmac."\n";
-
-										// $hmac_enc = base64_encode($hmac);
-
-
-
-
-										$curl = curl_init($serviceURL);
-										curl_setopt($curl, CURLOPT_HEADER, false);
-										curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-
-										curl_setopt($curl, CURLOPT_POST, true);
-										curl_setopt($curl, CURLOPT_POSTFIELDS, $reqbody);
-										curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
-										curl_setopt($curl, CURLOPT_VERBOSE, true);
-
-
-										$headers = array(
-											'Content-type: application/json',
-											'token: fdoa-31313137a2ea512a73d0f440a2b049f7977cf2899d8f3729',
-											"Authorization: ".$authorization,
-											"apikey: ".$apikey,
-											"token: ".$token,
-											"timestamp: ".$timestamp,
-											"nonce: ".$nonce,
+										$cardDetails =array(
+											'type'=>$_POST['cardtype'],
+											'cardholder_name'=> $_POST['payname'],
+											'card_number'=> $_POST['cardnumber'],
+											'exp_date'=> $expDate,
+											'cvv'=> $_POST['cvv']
 										);
 
-										curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+										$arr=array(											 																							
+												'transaction_type'=> 'authorize',
+												'method'=> 'credit_card',
+												'amount'=> $_POST['amount'],
+												'currency_code'=> 'USD',
+												'credit_card'=> $cardDetails
+										);
 
-										$json_response = curl_exec($curl);
+										$apiKey = "Kyv2tGF0nSVGKLTkHnjyrbwLwqhmpzSU";
+										$apiSecret = "2c817fa94e5240af0f59bb31d4e89a8342905db75ae41f074f7ff2ba4ec6b0ac";
+										$nonce = getrandmax();
+										$nonce .= getrandmax();
 
-										$status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+										list($msec, $sec) = explode(' ', microtime());
 
-										// if ( $status != 201 ) {
-										//     die("Error: call to URL $serviceURL failed with status $status, response $json_response, curl_error " . curl_error($curl) . ", curl_errno " . curl_errno($curl));
-										// }
+										$timestamp =  (int) ($sec.substr($msec, 2, 3));
 
+										$token = "fdoa-25fb9b0b7a498aa19e5816d50d15d0a1c885971a73c27f9d";
+										$payload = json_encode($arr);
+
+										$data = $apiKey;
+										$data.= $nonce;
+										$data.= $timestamp; 
+										$data.= $token;
+										$data.= $payload;
+
+										$hashAlgorithm = "sha256";
+
+										$hmac = hash_hmac ( $hashAlgorithm , $data , $apiSecret, false );
+										
+										$authorization = base64_encode($hmac);
+
+										
+										echo $timestamp;
+										echo '<br>';
+										echo $nonce;
+										echo '<br>';
+										echo $authorization;
+
+										$auth=array(
+											'apikey'=> $apiKey,
+											'nonce'=>$nonce,
+											'timestamp0'=>$timestamp,
+											'token'=> $token,
+											'Authorization'=> $authorization,
+											'Content-Type: application/json'
+										);
+												
+										$curl = curl_init();
+
+										curl_setopt_array($curl, array(
+										CURLOPT_URL => 'https://api-cert.payeezy.com/v1/transactions',
+										CURLOPT_RETURNTRANSFER => true,
+										CURLOPT_ENCODING => '',
+										CURLOPT_MAXREDIRS => 10,
+										CURLOPT_TIMEOUT => 0,
+										CURLOPT_FOLLOWLOCATION => true,
+										CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+										CURLOPT_CUSTOMREQUEST => 'POST',
+										CURLOPT_POSTFIELDS =>json_encode( $arr ),
+										CURLOPT_HTTPHEADER =>$auth,
+										));
+
+										$response = curl_exec($curl);
 
 										curl_close($curl);
-										$response = json_decode($json_response, true);
-										// echo "Response is: ".$response."\n";
-										echo "JSON response is: ".$json_response."\n";
-										$redorangepayment = $response;
-										if ($redorangepayment->validation_status == "success"){
+										// echo $response;
+
+										$redorangepayment = json_decode($response,true) ;
+										if ($redorangepayment['validation_status'] == "success"){
 
 											echo '<div class="alert alert-success" role="alert">';
 											echo "<p><strong>Thank For Payment</strong></p>";
-											echo "<p><strong>Payment Status</strong>: ".$redorangepayment->validation_status."</p>";
-											echo "<p><strong>Transaction ID</strong>: ".$redorangepayment->transaction_id."</p>";
+											echo "<p><strong>Payment Status</strong>: ".$redorangepayment['validation_status']."</p>";
+											echo "<p><strong>Transaction ID</strong>: ".$redorangepayment['transaction_id']."</p>";
 											echo "</div>";
 										} else {
 											echo '<div class="alert alert-danger" role="alert">';
 											echo "<p><strong>Payment Status</strong>: Payment Failed</p>";
+											echo $response;
 											// echo "<p><strong>Payment Status</strong>: ".$redorangepayment['Error']['messages']['code']."</p>";
-											// echo "<p><strong>Transaction ID</strong>: ".$redorangepayment->transaction_id."</p>";
+											// echo "<p><strong>Transaction ID</strong>: ".$redorangepayment['transaction_id']."</p>";
 											echo "</div>";
 										}
-										?> 
+									?>
                                     </div>                  								
 								</div>
 							</div><!-- column end -->
